53de64149f2ab28b6b64ac9e859a30185ee958b0
Joseph <jkb5j@mtmail.mtsu.edu>
2019-07-23 21:42:55 -0400

    done

--- Ginyu-Force-reimbursement-api/src/routers/reimbursements.router.ts
+++ Ginyu-Force-reimbursement-api/src/routers/reimbursements.router.ts
@@ -1,7 +1,5 @@
 /**
  * All requests to this router will come from /reimbursements
- * This router handles all requests to interact with the
- * reimbursements.
  */
 
 import express from 'express';
@@ -13,10 +11,8 @@ export const reimbursementsRouter = express.Router();
 
 /**
  * This returns all reimbursements by their status. This may
- * be accessed by 'Captain', 'Sub Captain'. It
- * should sort by the date submitted.
+ * be accessed by 'Captain', 'Sub Captain'.
  *
- * CHALLENGE: Have the DB sort using
  * /reimbursements/status/:statudId
  */
 reimbursementsRouter.get('/status/:statusId', [authMiddleware('Captain', 'Sub Captain'),
@@ -34,10 +30,7 @@ async (req, res) => {
  * This returns all reimbursements by a single user. This may
  * be accessed by 'Captain', 'Sub Captain', or if
  * the user ID matches the reimbursement user ID. It should be
- * sorted by date submitted.
  *
- * CHALLENGE: As above
- *  /reimbursements/author/userId/:userId/date-submitted?start=:startDate&end=:endDate
  */
 
 
@@ -56,7 +49,6 @@ async (req, res) => {
 /**
  * This allows a user to create a reimbursement. It will return
  * a status code of 201 CREATED if successful. ReimbursementID
- * should be sent as 0, and the DB will serialize it.
  */
 reimbursementsRouter.post('', [authMiddleware('Captain', 'Sub Captain', 'Grunt'),
 async (req, res) => {
@@ -75,8 +67,6 @@ async (req, res) => {
 /**
  * This allows a Captain or Sub Captain to update the
  * status of the reimbursement. ReimbursementID must be the same
- * as the DB, but any missing fields will not be updated. Returns
- * the updated reimbursement.
  */
 reimbursementsRouter.patch('', [authMiddleware('Captain', 'Sub Captain'),
 async (req, res) => {
